467fac45e1bf67cd4780f9877d066eafef48fcf4
FMABr <felipemuamorim@gmail.com>
2022-11-04 18:50:02 -0400

    Printer Info page and viewModel

---
+++ xamarin-lib-harpia/ViewModels/InfoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using xamarin_lib_harpia.Models;
+
+namespace xamarin_lib_harpia.ViewModels
+{
+    internal class InfoViewModel : INotifyPropertyChanged
+    {
+        PrinterInfo printerInfo;
+        PackageInfo packageInfo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public InfoViewModel()
+        {
+            // Carregar dados da impressora atualmente conectada
+            printerInfo = new PrinterInfo();
+
+            // Carregar dados da dependência da Sunmi
+            packageInfo = new PackageInfo();
+        }
+
+        public PrinterInfo PrinterInfo { get { return printerInfo; } }
+        public PackageInfo PackageInfo { get { return packageInfo; } }
+    }
+}
