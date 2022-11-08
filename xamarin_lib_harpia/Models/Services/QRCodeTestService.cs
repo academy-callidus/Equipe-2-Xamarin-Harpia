2011bf156e9564e0d5b7732432cb3370632510d1
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-11-07 22:50:56 -0400

    [test task] add QRCodeTestService

---
+++ xamarin_lib_harpia/Models/Services/QRCodeTestService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarin_lib_harpia.Models.Services
+{
+
+    internal class QRCodeTestService
+    {
+        private int ModuleSize { set; get; }
+        private int ErrorLevel { set; get; }
+
+        public QRCodeTestService(int moduleSize, int errorLevel)
+        {
+            ModuleSize = moduleSize;
+            ErrorLevel = errorLevel;
+        }
+
+        public void PrintQRCode(int align, string dataContent_1, string data_Content_2) // enum de alinhamento
+        {
+
+        }
+    }
+}
