c7cc1c462adae1db1c17c79a74ed2b4795194aaa
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2022-11-08 13:18:55 -0300

    TextService initial commit

---
+++ xamarin_lib_harpia/Models/Entities/Text.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarin_lib_harpia.Models.Entities
+{
+    public class Text
+    {
+        private string Content { get; set; }
+        private int Record { get; set; }
+        private bool IsBold { get; set; }
+        private bool IsUnderline { get; set; }
+        private bool IsK1 { get; set; }
+        private float Width { get; set; }
+        private float Height { get; set; }
+        private string[] MStrings { get; set; }
+
+        public Text(string content, int record, bool isBold, bool isUnderline, bool isK1, float width, float height, string[] mStrings)
+        {
+            Content = content;
+            Record = record;
+            IsBold = isBold;
+            IsUnderline = isUnderline;
+            IsK1 = isK1;
+            Width = width;
+            Height = height;
+            MStrings = mStrings;
+        }
+    }
+}
---
+++ xamarin_lib_harpia/Models/Services/TextService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xamarin_lib_harpia.Models.Entities;
+
+namespace xamarin_lib_harpia.Models.Services
+{
+    public class TextService
+    {
+        private IPrinterConnection Connection;
+
+        public TextService(IPrinterConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public bool execute(Text text)
+        {
+
+            return true;
+        }
+    }
+}
