e036616a3ce3788bb252dc4b125d6784b901d19c
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-11-07 22:50:28 -0400

    [test task] add BarcodeTestService

---
+++ xamarin_lib_harpia/Models/Services/BarcodeTestService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xamarin_lib_harpia.Utils;
+
+namespace xamarin_lib_harpia.Models.Services
+{
+    internal class BarcodeTestService
+    {
+
+        readonly TectoySunmiPrint printerInstance = TectoySunmiPrint.getInstance();
+        private int height {
+            set;
+            get;
+        }
+        private int width { set; get; }
+        private int advanceLines { set; get; }
+        private int barcodeModel { set; get; } // adicionar BarcodeModelEnum
+
+        public BarcodeTestService(int width, int height, int advanceLines, int barcodeModel)
+        {
+            this.width = width;
+            this.height = height;
+            this.advanceLines =advanceLines;
+            this.barcodeModel =barcodeModel;
+        }
+
+       
+        public void PrintTextBelow(string text)
+        {
+            
+        }
+
+        public void PrintTextAbove(string text)
+        {
+
+        }
+
+        public void PrintTextBelowAndAbove(string text)
+        {
+
+        }
+
+
+
+    }
+}
