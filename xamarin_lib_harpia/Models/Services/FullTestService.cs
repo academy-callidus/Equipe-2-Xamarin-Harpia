282079ff18407948d1e704c425b6f44582468d61
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-11-07 22:51:21 -0400

    [test task] add FullTestService

---
+++ xamarin_lib_harpia/Models/Services/FullTestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarin_lib_harpia.Models.Services
+{
+    internal class FullTestService
+    {
+        BarcodeTestService TestBarcode = new BarcodeTestService(0, 0, 0, 0);
+        QRCodeTestService TestQRCode = new QRCodeTestService(0, 0);
+        TextTestService TestText = new TextTestService();
+
+        public void TestAll()
+        {
+            TestBarcode.PrintTextAbove("00000000");
+            TestBarcode.PrintTextBelow("00000000");
+            TestBarcode.PrintTextBelowAndAbove("00000000");
+
+            TestQRCode.PrintQRCode(0, "www.tectoy.com.br", "harpia");
+
+            TestText.AlignText();
+            TestText.TextPrintShapes();
+
+
+        }
+    }
+}
