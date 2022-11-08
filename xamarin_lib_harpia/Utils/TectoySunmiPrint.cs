215ffdd4a76be90e9be51dbccd8775fb45a8235a
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-11-07 22:50:09 -0400

    [test task] add empty methods to TectoySunmiPrinter

--- xamarin_lib_harpia/Utils/TectoySunmiPrint.cs
+++ xamarin_lib_harpia/Utils/TectoySunmiPrint.cs
@@ -6,7 +6,27 @@ namespace xamarin_lib_harpia.Utils
 {
     public class TectoySunmiPrint
     {
-        
+        private static TectoySunmiPrint helper = new TectoySunmiPrint();
+
+        public static TectoySunmiPrint getInstance()
+        {
+            return helper;
+        }
+
+        public void SetSize(int align)
+        {
+
+        }
+
+        public void SetAlign(int align)
+        {
+            
+        }
+
+        public void PrintText(string text)
+        {
+
+        }
 
     }
 }
