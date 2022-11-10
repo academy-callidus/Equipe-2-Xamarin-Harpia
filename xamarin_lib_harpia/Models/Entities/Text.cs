5ce7cd11bad5b8c5b54e3653e210f15712178ac7
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2022-11-10 13:55:52 -0300

    Getters Setters Changes

--- xamarin_lib_harpia/Models/Entities/Text.cs
+++ xamarin_lib_harpia/Models/Entities/Text.cs
@@ -6,13 +6,41 @@ namespace xamarin_lib_harpia.Models.Entities
 {
     public class Text
     {
-        private string Content { get; set; }
-        private int Record { get; set; }
-        private bool IsBold { get; set; }
-        private bool IsUnderline { get; set; }
-        private bool IsK1 { get; set; }
-        private float Width { get; set; }
-        private float Height { get; set; }
+        private string Content 
+        { 
+            get { return Content; } 
+            set { Content = value; } 
+        }
+        private int Record 
+        { 
+            get { return Record } 
+            set { Record = value; } 
+        }
+        private bool IsBold 
+        { 
+            get { return IsBold; }
+            set { IsBold = value; } 
+        }
+        private bool IsUnderline 
+        { 
+            get { return IsUnderline; } 
+            set { IsUnderline = value; } 
+        }
+        private bool IsK1 
+        { 
+            get { return IsK1; } 
+            set { IsK1 = value; }
+        }
+        private float Width 
+        {
+            get { return Width; }
+            set { Width = value; }
+        }
+        private float Height 
+        { 
+            get { return Height; } 
+            set { Height = value; } 
+        }
         private string[] MStrings { get; set; }
 
         public Text(string content, int record, bool isBold, bool isUnderline, bool isK1, float width, float height, string[] mStrings)
