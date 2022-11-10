ea367c10fef7dec777451687e900f0703e7d5167
artuguen28 <87493045+artuguen28@users.noreply.github.com>
2022-11-10 18:28:41 +0100

    Execute() method updated

--- xamarin_lib_harpia/Models/Services/QRCodeService.cs
+++ xamarin_lib_harpia/Models/Services/QRCodeService.cs
@@ -14,50 +14,18 @@ namespace xamarin_lib_harpia.Models.Services
         {
             Connection = connection;
         }
-        public bool Execute(QRcode qrcode)
+        public async Task<bool> Execute(QRcode qrcode)
         {
-            /*if (HasCut)
+            if (!qrcode.IsValid()) return false;
+            try
             {
-                if (true isK1 = true && height > 1856)
-                {
-                    //QRCodeService QRService = new QRCodeService();
-                    //QRService.setAlign(1);
-                    //QRService.text("QrCode\n");
-                    //QRService.text("--------------------------------\n");
-                    //QRService.printQr(mTextView1.getText().toString(), print_size, error_level);
-                    //QRService.print3Line();
-                    //QRService.cutpaper(KTectoySunmiPrinter.HALF_CUTTING, 10);
-                }
-                else
-                {
-                    //TectoySunmiPrint.getInstance().setAlign(TectoySunmiPrint.Alignment_CENTER);
-                    //TectoySunmiPrint.getInstance().printText("QrCode\n");
-                    //TectoySunmiPrint.getInstance().printText("--------------------------------\n");
-                    //TectoySunmiPrint.getInstance().printQr(mTextView1.getText().toString(), print_size, error_level);
-                    //TectoySunmiPrint.getInstance().print3Line();
-                }
-            } else
+                var response = await Connection.PrintQRCode(qrcode);
+                return response;
+            }
+            catch(Exception exception)
             {
-                if (true isK1 = true && height > 1856)
-                {
-                    //kPrinterPresenter.setAlign(1);
-                    //kPrinterPresenter.text("QrCode\n");
-                    //kPrinterPresenter.text("--------------------------------\n");
-                    //kPrinterPresenter.printQr(mTextView1.getText().toString(), print_size, error_level);
-                    //kPrinterPresenter.print3Line();
-                    //kPrinterPresenter.cutpaper(KTectoySunmiPrinter.HALF_CUTTING, 10);
-                }
-                else
-                {
-                    //TectoySunmiPrint.getInstance().setAlign(TectoySunmiPrint.Alignment_CENTER);
-                    //TectoySunmiPrint.getInstance().printText("QrCode\n");
-                    //TectoySunmiPrint.getInstance().printText("--------------------------------\n");
-                    //TectoySunmiPrint.getInstance().printQr(mTextView1.getText().toString(), print_size, error_level);
-                    //TectoySunmiPrint.getInstance().print3Line();
-                    //TectoySunmiPrint.getInstance().cutpaper();
-                }
-            }*/
-            return true;
+                Console.WriteLine(exception.Message);
+            }
         }
 
         public bool InitConnection()
