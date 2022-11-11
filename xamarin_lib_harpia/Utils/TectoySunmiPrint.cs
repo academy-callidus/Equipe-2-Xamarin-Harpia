43ba68b12553df56357248859ee45470778cebf7
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-11-11 01:26:09 -0400

    [test task] add barcodeTest in FullTest

--- xamarin_lib_harpia/Utils/TectoySunmiPrint.cs
+++ xamarin_lib_harpia/Utils/TectoySunmiPrint.cs
@@ -7,27 +7,7 @@ namespace xamarin_lib_harpia.Utils
 {
     public class TectoySunmiPrint
     {
-        private static TectoySunmiPrint helper = new TectoySunmiPrint();
 
-        public static TectoySunmiPrint getInstance()
-        {
-            return helper;
-        }
-
-        public void SetSize(int align)
-        {
-
-        }
-
-        public void SetAlign(AlignmentEnum align)
-        {
-            
-        }
-
-        public void PrintText(string text)
-        {
-
-        }
 
     }
 }
