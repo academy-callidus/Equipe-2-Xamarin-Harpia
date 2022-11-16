dc31ea9a49aa639caed1dd38ae991adb8510409e
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-11-16 18:39:07 -0400

    refactoring titlebar

---
+++ xamarin_lib_harpia/ViewModels/TitlebarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using xamarin_lib_harpia.Views;
+
+namespace xamarin_lib_harpia.ViewModels
+{
+    internal class TitlebarViewModel
+    {
+       
+    }
+}
