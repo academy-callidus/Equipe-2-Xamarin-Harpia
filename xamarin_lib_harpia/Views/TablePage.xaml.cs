3a99b2416970ade9d6fd33958a83454a3b2bc144
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2022-11-21 14:44:41 -0300

    TableItem creation

--- xamarin_lib_harpia/Views/TablePage.xaml.cs
+++ xamarin_lib_harpia/Views/TablePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,31 @@ namespace xamarin_lib_harpia.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TablePage : ContentPage
     {
+        ArrayList datalist = new ArrayList();
+
         public TablePage()
         {
             InitializeComponent();
+        }
+
+        void initList(ArrayList data)
+        {
+            TableItem ti = new TableItem();
+            data.Add(ti);
+        }
+    }
+
+     class TableItem
+    {
+        public string[] Text { get; set; }
+        public int[] Weigth { get; set; }
+        public string[] Align { get; set; }
+
+        public TableItem()
+        {
+            Text = new string[] { "texto" , "texto", "texto" };
+            Weight = new int[] { 1, 1, 1 };
+            Align = new string[] { "Esquerda", "Esquerda", "Esquerda" };
         }
     }
 }
