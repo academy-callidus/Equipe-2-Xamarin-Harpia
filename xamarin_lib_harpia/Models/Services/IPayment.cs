3b711f71f5520a634491dfb315f54ece23d8c687
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2022-12-06 09:32:57 -0400

    add payment interface

---
+++ xamarin_lib_harpia/Models/Services/IPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarin_lib_harpia.Models.Services
+{
+    public interface IPayment
+    {
+        bool MakeAdminTransition(); 
+        bool MakeSaleTransition(); 
+        bool CancelPayment();
+    }
+}
